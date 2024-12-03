c56aa681834c923084634e69faadc1700ee85812
luizlipe <felipe250718@gmail.com>
2024-12-03 09:14:31 -0300

    feat: adiciona função para mudar as cores

---
+++ script.js
@@ -0,0 +1,13 @@
+let currentColor = 'black'
+
+document.querySelectorAll('.colorArea .color').forEach(item => {
+    item.addEventListener('click', colorClickEvent)
+})
+
+function colorClickEvent(e){
+    let color = e.target.getAttribute('data-color')
+    console.log("Cor clicada: ", color)
+
+    document.querySelector('.color.active').classList.remove('active')
+    e.target.classList.add('active')
+}
