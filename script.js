63f763e0cd4ee56e2715382b8e6021fa8cb12cc2
luizlipe <felipe250718@gmail.com>
2024-12-04 10:57:34 -0300

    feat: adicionada função de desenhar no canva

--- script.js
+++ script.js
@@ -1,13 +1,59 @@
+//Initial Data
 let currentColor = 'black'
+let canDraw = false
+let mouseX = 0
+let mouseY = 0
 
+let screen = document.querySelector('#tela')
+let ctx = screen.getContext('2d')
+
+//Events
 document.querySelectorAll('.colorArea .color').forEach(item => {
     item.addEventListener('click', colorClickEvent)
 })
 
+screen.addEventListener('mousedown', mouseDownEvent)
+screen.addEventListener('mousemove', mouseMoveEvent)
+screen.addEventListener('mouseup', mouseUpEvent)
+
+
+
+//Functions
 function colorClickEvent(e){
     let color = e.target.getAttribute('data-color')
-    console.log("Cor clicada: ", color)
+    currentColor = color
 
     document.querySelector('.color.active').classList.remove('active')
     e.target.classList.add('active')
+}
+
+function mouseDownEvent(e){
+    canDraw = true
+    mouseX = e.pageX - screen.offsetLeft
+    mouseY = e.pageY - screen.offsetTop
+}
+function mouseMoveEvent(e){
+    if(canDraw){
+        draw(e.pageX, e.pageY)
+    }
+}
+
+function mouseUpEvent(){
+    canDraw = false
+}
+function draw(x, y){
+    let pointX = x - screen.offsetLeft
+    let pointY = y - screen.offsetTop
+
+    ctx.beginPath()
+    ctx.lineWidth = 5
+    ctx.lineJoin = 'round'
+    ctx.moveTo(mouseX, mouseY)
+    ctx.lineTo(pointX, pointY)
+    ctx.closePath() 
+    ctx.strokeStyle = currentColor
+    ctx.stroke()
+
+    mouseX = pointX
+    mouseY = pointY
 }
